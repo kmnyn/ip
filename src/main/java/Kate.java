f46d8504a6c477858b58ebdd60b5b6b452b07a6b
kmnyn <mengyan717@gmail.com>
2025-02-06 02:15:38 +0800

    rename bot and update greeting

---
+++ src/main/java/Kate.java
@@ -0,0 +1,13 @@
+public class Kate {
+    public static void main(String[] args) {
+        // Greet the user
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Kate");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Exit message
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
