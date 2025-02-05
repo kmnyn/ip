04c1a9ddfa53e1102e439abd398ef9aec1ce1c95
kmnyn <mengyan717@gmail.com>
2025-02-06 02:49:37 +0800

    Add task management function

---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    // Constructor to initialize a task with a description
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false; // Tasks are not done by default
+    }
+
+    // Method to mark the task as done
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    // Method to mark the task as not done
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    // Method to return the status icon based on whether the task is done
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // Display [X] for done, [ ] for not done
+    }
+
+    // Getter for the task description
+    public String getDescription() {
+        return description;
+    }
+}
+
+
