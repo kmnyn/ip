744c3310b9a2dfe337f099a72429c11e2378e4d4
kmnyn <mengyan717@gmail.com>
2025-02-07 14:34:08 +0800

    Level-1: Echo

--- src/main/java/Kate.java
+++ src/main/java/Kate.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Kate {
     public static void main(String[] args) {
         // Greet the user
@@ -5,8 +7,30 @@ public class Kate {
         System.out.println("Hello! I'm Kate");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        // Exit message
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Start an infinite loop to continuously ask for user input
+        while (true) {
+            // Read the user's command
+            String input = scanner.nextLine();
+
+            // If the input is "bye", break out of the loop and exit
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. See ya!");
+                System.out.println("____________________________________________________________");
+                break;  // Exit the program
+            }
+
+            // Otherwise, echo the user's command
+            System.out.println("____________________________________________________________");
+            System.out.println(input);  // Echo the command
+            System.out.println("____________________________________________________________");
+        }
+
+        // Close the scanner to prevent resource leaks
+        scanner.close();
     }
 }
