039deb3c8d1db82f80cd87fdbfab05788aee45ff
kmnyn <mengyan717@gmail.com>
2025-02-07 14:55:55 +0800

    Level-3: Mark as done

--- src/main/java/Kate.java
+++ src/main/java/Kate.java
@@ -12,7 +12,7 @@ public class Kate {
         Scanner scanner = new Scanner(System.in);
 
         // Create an array to store tasks (fixed size of 100 as per instruction)
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         // Start an infinite loop to continuously ask for user input
@@ -31,19 +31,52 @@ public class Kate {
             // If the input is "list", show all stored tasks
             if (input.equalsIgnoreCase("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
                 // Display the list of tasks
                 if (taskCount == 0) {
                     System.out.println("No tasks added yet.");
                 } else {
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ". " + tasks[i]);
+                        System.out.println((i + 1) + "." + tasks[i].getStatusIcon() + " " + tasks[i].getDescription());
                     }
                 }
                 System.out.println("____________________________________________________________");
+            } else if (input.startsWith("mark")) {
+                // Mark a task as done
+                try {
+                    int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1; // Get the task index
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        tasks[taskIndex].markAsDone();
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println(tasks[taskIndex].getStatusIcon() + " " + tasks[taskIndex].getDescription());
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        System.out.println("Task not found.");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid task number.");
+                }
+            } else if (input.startsWith("unmark")) {
+                // Unmark a task (set it back to not done)
+                try {
+                    int taskIndex = Integer.parseInt(input.split(" ")[1]) - 1; // Get the task index
+                    if (taskIndex >= 0 && taskIndex < taskCount) {
+                        tasks[taskIndex].unmark();
+                        System.out.println("____________________________________________________________");
+                        System.out.println("OK, I've marked this task as not done yet:");
+                        System.out.println(tasks[taskIndex].getStatusIcon() + " " + tasks[taskIndex].getDescription());
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        System.out.println("Task not found.");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid task number.");
+                }
             } else {
                 // Otherwise, add the input as a new task
                 if (taskCount < 100) {
-                    tasks[taskCount] = input;  // Store the task in the array
+                    tasks[taskCount] = new Task(input);  // Create a new task and add it
                     taskCount++;  // Increment task count
                     System.out.println("____________________________________________________________");
                     System.out.println("added: " + input);  // Confirm the task has been added
