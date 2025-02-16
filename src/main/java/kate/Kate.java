171ef78ae3796eefdb89b0c9f3680d6b17aae528
kmnyn <mengyan717@gmail.com>
2025-02-17 00:02:22 +0800

    Implement delete command

--- src/main/java/kate/Kate.java
+++ src/main/java/kate/Kate.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.ArrayList;
 
 // Importing commands
 import command.Command;
@@ -18,9 +19,7 @@ import task.Event;
 
 public class Kate {
 
-    private static final int MAX_TASKS = 100;
-    private static final Task[] tasks = new Task[MAX_TASKS];
-    private static int taskCount = 0;
+    public static final ArrayList<Task> tasks = new ArrayList<>();
 
     private static final Logger logger = Logger.getLogger(Kate.class.getName());
 
@@ -74,47 +73,47 @@ public class Kate {
     }
 
     public static void listTasks() {
-        if (taskCount == 0) {
+        if (tasks.isEmpty()) {
             System.out.println("Your task list is empty.");
             return;
         }
         System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < taskCount; i++) {
-            System.out.println((i + 1) + "." + tasks[i]);
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + "." + tasks.get(i));
         }
     }
 
     public static void addTodo(String description) {
-        tasks[taskCount++] = new Todo(description);
-        System.out.println("Got it. I've added this task:\n  " + tasks[taskCount - 1]);
-        System.out.println("Now you have " + taskCount + " tasks in the list.");
+        tasks.add(new Todo(description));
+        System.out.println("Got it. I've added this task:\n  " + tasks.get(tasks.size() - 1));
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
     }
 
     public static void addDeadline(String description, String by) {
-        tasks[taskCount++] = new Deadline(description, by);
-        System.out.println("Got it. I've added this task:\n  " + tasks[taskCount - 1]);
-        System.out.println("Now you have " + taskCount + " tasks in the list.");
+        tasks.add(new Deadline(description, by));
+        System.out.println("Got it. I've added this task:\n  " + tasks.get(tasks.size() - 1));
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
     }
 
     public static void addEvent(String description, String from, String to) {
-        tasks[taskCount++] = new Event(description, from, to);
-        System.out.println("Got it. I've added this task:\n  " + tasks[taskCount - 1]);
-        System.out.println("Now you have " + taskCount + " tasks in the list.");
+        tasks.add(new Event(description, from, to));
+        System.out.println("Got it. I've added this task:\n  " + tasks.get(tasks.size() - 1));
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
     }
 
     public static void markTask(int index) {
-        if (index >= 0 && index < taskCount) {
-            tasks[index].markAsDone();
-            System.out.println("Nice! I've marked this task as done:\n  " + tasks[index]);
+        if (index >= 0 && index < tasks.size()) {
+            tasks.get(index).markAsDone();
+            System.out.println("Nice! I've marked this task as done:\n  " + tasks.get(index));
         } else {
             System.out.println("Invalid task number.");
         }
     }
 
     public static void unmarkTask(int index) {
-        if (index >= 0 && index < taskCount) {
-            tasks[index].markAsNotDone();
-            System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[index]);
+        if (index >= 0 && index < tasks.size()) {
+            tasks.get(index).markAsNotDone();
+            System.out.println("OK, I've marked this task as not done yet:\n  " + tasks.get(index));
         } else {
             System.out.println("Invalid task number.");
         }
